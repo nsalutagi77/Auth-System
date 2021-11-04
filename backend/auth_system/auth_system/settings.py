d7ad537ee6828c093aab53e5b81cab0ff2ca786c
nsalutagi77 <43502374+nsalutagi77@users.noreply.github.com>
2021-11-04 18:08:31 +0530

    Update settings.py

--- backend/auth_system/auth_system/settings.py
+++ backend/auth_system/auth_system/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'naveengks77@gmail.com'
-EMAIL_HOST_PASSWORD = 'adrxyoakpraypldb'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = 'adrxyoadb'
 EMAIL_USE_TLS = True
 
 # Password validation
